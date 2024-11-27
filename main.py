ca8b4b1f0a75039480e9eda32cd8eb6e46f70ff1
carlosdbb14 <32582561+carlosdbb14@users.noreply.github.com>
2024-11-27 17:54:00 -0500

    Update main.py
    comentarios estructurando como sera el codigo

--- main.py
+++ main.py
@@ -12,3 +12,17 @@ df = df.withColumn('Timestamp', to_timestamp(df['Timestamp'],'yyyy/MM/dd HH:mm')
 df.show()
 
 df.printSchema()
+
+
+## hacer analisis exploratorio de datos, mirar cuantos fraudes hay, con que monedas, cuantos datos hay de cada moneda, cuanto dinero se mueve, cuantos no fraudulentos hay, relaciones entre variables, etc, etc, eso se puede dejar en planos con los datos resumidos (ojala, los planos unidos)
+
+
+#modelos para compararlos
+
+#hacer un random forest
+
+#hacer un xgboost
+
+#comparar los dos
+
+
